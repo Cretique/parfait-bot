15c8a46075454223a65c6d189e41720ab13882a3
Batın <58427283+emirbatin@users.noreply.github.com>
2024-08-22 15:06:15 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,23 +1,25 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require("discord.js");
+const { clientId } = require("../config.js");
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('invite')
-        .setDescription('Generates an invite link for the bot with administrator permissions.'),
-    async execute(interaction) {
-        const clientId = '1225564511717818408';
-        const inviteLink = `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot%20applications.commands`;
+  data: new SlashCommandBuilder()
+    .setName("invite")
+    .setDescription(
+      "Generates an invite link for the bot with administrator permissions."
+    ),
+  async execute(interaction) {
+    const inviteLink = `https://discord.com/oauth2/authorize?client_id=${clientId}&permissions=8&scope=bot%20applications.commands`;
 
-        await interaction.reply({
-            embeds: [
-                {
-                    color: 0x00AE86,
-                    title: 'Invite Link',
-                    description: `Click [here](${inviteLink}) to invite the bot with Administrator permissions.`,
-                    footer: { text: 'Invite your bot to your server!' }
-                }
-            ],
-            ephemeral: true
-        });
-    },
+    await interaction.reply({
+      embeds: [
+        {
+          color: 0x00ae86,
+          title: "Invite Link",
+          description: `Click [here](${inviteLink}) to invite the bot with Administrator permissions.`,
+          footer: { text: "Invite your bot to your server!" },
+        },
+      ],
+      ephemeral: true,
+    });
+  },
 };
